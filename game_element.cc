38e7861146e22a2c16924c236d7b48bb66ab6ecd
justdiam12 <justdiam12@csu.fullerton.edu>
2021-05-01 01:19:43 +0000

    In Progress.

--- game_element.cc
+++ game_element.cc
@@ -7,9 +7,9 @@ bool GameElement::IsOutOfBounds(const graphics::Image& screen) {
          GetY() + GetHeight() > screen.GetHeight();
 }
 
-bool GameElement::IntersectsWith(const GameElement& other) {
-  return !(GetX() > other.GetX() + other.GetWidth() ||
-           other.GetX() > GetX() + GetWidth() ||
-           GetY() > other.GetY() + other.GetHeight() ||
-           other.GetY() > GetY() + GetHeight());
+bool GameElement::IntersectsWith(GameElement* other) {
+  return !(GetX() > (*other).GetX() + (*other).GetWidth() ||
+           (*other).GetX() > GetX() + GetWidth() ||
+           GetY() > (*other).GetY() + (*other).GetHeight() ||
+           (*other).GetY() > GetY() + GetHeight());
 }
